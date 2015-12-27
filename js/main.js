33566555a59d508051e1d6738cde2ae6ad57c97a
Noah Paessel <knowuh@gmail.com>
2015-12-27 16:53:36 -0500

    WIP:  Getting course setup.

---
+++ js/main.js
@@ -0,0 +1,31 @@
+(function() {
+
+  var formatDay = function(selection) {
+    selection
+      .append("span").attr("class", "date")
+        .text(function(d) { return d.date;})
+      .append("span").attr("class", "title")
+        .text(function(d) { return d.title;})
+      .append("span").attr("class", "description")
+        .text(function(d) { return d.description;});;
+  };
+
+  var makeLinks=function(dataLinks) {
+    // var ul = $("#content").append("<ul>");
+    d3.select("#schedule").selectAll("li")
+      .data(dataLinks)
+      .enter()
+        .append("li")
+        .attr("class", "day")
+        .call(formatDay);
+    console.log("links made");
+    console.log(dataLinks);
+  };
+
+
+  $(document).ready(function() {
+    var data = $.getJSON("data/class-weeks.json", makeLinks);
+    makeLinks(data);
+  });
+
+})();
