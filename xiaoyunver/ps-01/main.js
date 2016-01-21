cad21a7eeee25fb17be5fe67d9640cb75806efa6
xiaoyunver <xinhe.yu@husky.neu.edu>
2016-01-20 20:37:21 -0500

    Nine Circles
    change to greyscale

--- xiaoyunver/ps-01/main.js
+++ xiaoyunver/ps-01/main.js
@@ -6,15 +6,15 @@ var c=document.getElementById("canvas");
 var circle=c.getContext('2d');
 circle.beginPath();
 circle.arc(50,50,50,0,2*Math.PI);
-circle.fillStyle="#CFACDB";
+circle.fillStyle="#000000";
 circle.fill();
 
 circle.beginPath();
 circle.arc(152,50,50,0,2*Math.PI);
-circle.fillStyle="#DBA0C0";
+circle.fillStyle="#494949";
 circle.fill();
 
 circle.beginPath();
 circle.arc(254,50,50,0,2*Math.PI);
-circle.fillStyle="#AEB9DB";
+circle.fillStyle="#CCCCCC";
 circle.fill();
