2c4acf87afd05152057d7486d5add78d63c2a8bc
Noah Paessel <knowuh@gmail.com>
2016-02-03 19:28:20 -0500

    Easier to read survey examples for google spreadsheet.

--- js/spreadsheet.js
+++ js/spreadsheet.js
@@ -1,3 +1,5 @@
+// Use this method to call your parentCallback function with the spreadhseet data
+// your `parentCallback` function should look like `var loadData = function(spreadsheetResults) { … }`
 window.loadSpreadsheet = function(spreadsheetID, parentCallback) {
   spreadsheetID = spreadsheetID || "1tL7m0JNa0CZwEyU9WmB3u8j5T829jqtbnu-26ibPp5E";
   parentCallback = parentCallback || function(data) {
@@ -9,7 +11,6 @@ window.loadSpreadsheet = function(spreadsheetID, parentCallback) {
   var version = "/1/public/values";
   var url =  baseUrl + spreadsheetID + version + callbackUrl;
 
-  var returnV = [];
   var mapValues = function(value) {
     var stringValues = _.trim(value.content.$t).split(',');
     var arrayValue = _.map(stringValues, function(s) {
@@ -32,4 +33,16 @@ window.loadSpreadsheet = function(spreadsheetID, parentCallback) {
       parentCallback(result);
     }
   });
+};
+
+// Use this function to call your parentCallback function every interval seconds
+// your function will receive the spreadsheet data as the first arugument.
+window.continuouslyLoadData = function(spreadheetID, parentCallback, interval) {
+  interval= interval || 5;
+  var intervalms = interval * 1000; // convert seconds to miliseconds
+  var getData = function() {
+    loadSpreadsheet(spreadheetID, parentCallback);
+  };
+  getData();
+  setInterval(getData, intervalms);
 };
