ca69a4d470a06c80b98cdfa9d31db906bdca080c
Luegreen <Luegreen@gmail.com>
2016-02-07 22:08:13 -0500

    orange outlines
    so many data reference changes that have not worked… still figuring it
    out…

--- Luegreen/ps_03/a/a.js
+++ Luegreen/ps_03/a/a.js
@@ -61,6 +61,16 @@ var setupSurvey =function() {
 
     var circles = svg.selectAll("circle")
       .data(data);
+      
+    var labels = svg.selectAll("text")
+      .data(data)
+      .enter()
+      .append("text")
+//    .attr("x", function(d,i) { return i * (w / data.length) + (w / data.length // ) })
+      .text(function(d)  {
+          return d;
+      })
+      
 
     circles
       .enter()
@@ -69,13 +79,17 @@ var setupSurvey =function() {
       .text(function (d) {
         return "name:" + d.name
       });
+      
 
     circles
       .attr('r', radius)
       .attr('fill', function (d, i) {
         var hue = colorScale(i);
-        return 'hsla(' + hue + ', 20%, 40%, 1.0)';
+        return 'hsla(' + hue + ', 30%, 80%, .8)';
       })
+      .attr("stroke", "orange")
+      .attr("stroke-width", function (d){return d/2;
+                                       })
       .attr("title", function (d) {
         return d.name;
       })
