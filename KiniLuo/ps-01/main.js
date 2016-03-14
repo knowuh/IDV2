ec64f18e073b62b694d7d7f85ccfc92475d9a103
Luegreen <Luegreen@gmail.com>
2016-03-14 19:59:10 -0400

    Working through many D3 iterations
    ps-03 is a D3 Survey test that I keep breaking and rebuilding for the
    sake of learning.

--- KiniLuo/ps-01/main.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by KiniLuo on 1/11/16.
- */
--- navarjun/ps-02/scripts/CanvasSizeHandler.js
+++
@@ -1,6 +0,0 @@
-var canvas = document.getElementById("drawingCanvas")
-var heading = document.getElementById("heading")
-var footer = document.getElementById("footer")
-
-canvas.height = window.innerHeight-heading.clientHeight-footer.clientHeight-footer.style.bottom-70
-canvas.width = window.innerWidth
