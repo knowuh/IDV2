db8a718307bf6547be2437558a9c0514794c5182
Noah Paessel <knowuh@gmail.com>
2016-03-23 06:04:50 -0400

    Minor improvements to extractor. Place plugin.

--- twitter/extractor/main.py
+++ twitter/extractor/main.py
@@ -2,14 +2,39 @@ import os
 import simplejson as json
 os.environ["AWS_PROFILE"] = "idv2"
 from extractor.query import Query
+from extractor.by_hour_query import ByHourQuery, parseTime
+
 from extractor.sentiment_plugin import SentimentPlugin
+from extractor.place_plugin import PlacePlugin
 from extractor.label_image import LabelImage
 
-searchTerm = "#foodporn"
 
-tweets = Query(searchTerm, limit=4, plugins=[SentimentPlugin(), LabelImage()])
-results = tweets.get_results()
-filename = "../sample/%(search_term)s.json" % {"search_term": searchTerm}
+def save(term, results):
+    filename = "../sample/%(search_term)s.json" % {"search_term": term}
+    with open(filename, 'w') as outfile:
+        json.dump(results, outfile)
 
-with open(filename, 'w') as outfile:
-    json.dump(results, outfile)
+
+def foodporn():
+    search_term = "#foodporn"
+    query = Query(search_term, limit=400, qFilter=Query.ImageFitler, plugins=[SentimentPlugin()])
+    save(search_term, query.get_results())
+
+
+def zika():
+    search_term = "#zika"
+    query = Query(search_term, limit=100, qFilter=Query.PlaceFilter, plugins=[PlacePlugin()])
+    tweets = query.get_results()
+    for tweet in tweets:
+        print tweet['country']
+    save(search_term, tweets)
+
+    # start_time = parseTime("02-18-2016 1:00")
+    # interval = 60 * 60
+    # for hour in range(0,24):
+    #     adjustment_hour = hour * interval
+    #     search = ByHourQuery(search_term, limit=20, start_time=start_time + adjustment_hour, interval=interval)
+    #     results = search.get_results()
+    #     print "hour: %(hour)s size: %(size)d" % {'hour': hour, 'size': len(results)}
+
+zika()
---
+++ twitter/extractor/place_plugin.py
@@ -0,0 +1,26 @@
+from extractor.utils import deep_fetch
+from random import uniform
+
+
+class PlacePlugin(object):
+
+    def random_coord(self, tweet):
+        tweet['x'] = None
+        tweet['y'] = None
+        xs = []
+        ys = []
+        if tweet.get('bounding_box'):
+            for coordinate in tweet.get('bounding_box')[0]:
+                xs.append(float(coordinate[0]))
+                ys.append(float(coordinate[1]))
+
+            tweet['x'] = uniform(min(xs), max(xs))
+            tweet['y'] = uniform(min(ys), max(ys))
+
+    def process(self, arrayOfTweets):
+        for i, tweet in enumerate(arrayOfTweets):
+            tweet['country'] = deep_fetch(tweet,'place.country')
+            tweet['city']    = deep_fetch(tweet,'place.city')
+            tweet['country_code'] = deep_fetch(tweet,'place.country_code')
+            tweet['bounding_box'] = deep_fetch(tweet,'place.bounding_box.coordinates')
+            self.random_coord(tweet)
