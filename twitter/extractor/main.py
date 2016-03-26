6a6d5a6d0608db894f37df2ac244425825e21e2f
Noah Paessel <knowuh@gmail.com>
2016-03-26 04:44:38 -0400

    Improvements to flicker processing. Modifications to extration.

--- twitter/extractor/main.py
+++ twitter/extractor/main.py
@@ -20,21 +20,18 @@ def foodporn():
     query = Query(search_term, limit=400, qFilter=Query.ImageFitler, plugins=[SentimentPlugin()])
     save(search_term, query.get_results())
 
+def fashion():
+    search_term = "#fashion"
+    query = Query(search_term, limit=50, qFilter=Query.ImageFitler)
+    save(search_term, query.get_results())
+
 
 def zika():
     search_term = "#zika"
     query = Query(search_term, limit=100, qFilter=Query.PlaceFilter, plugins=[PlacePlugin()])
     tweets = query.get_results()
     for tweet in tweets:
-        print tweet['country']
+        print tweet.get('country_code')
     save(search_term, tweets)
 
-    # start_time = parseTime("02-18-2016 1:00")
-    # interval = 60 * 60
-    # for hour in range(0,24):
-    #     adjustment_hour = hour * interval
-    #     search = ByHourQuery(search_term, limit=20, start_time=start_time + adjustment_hour, interval=interval)
-    #     results = search.get_results()
-    #     print "hour: %(hour)s size: %(size)d" % {'hour': hour, 'size': len(results)}
-
-zika()
+fashion()
